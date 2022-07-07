1c00a0b075cd25970c5dab8f3b3bdefcff97a4f4
hoon <hoonnotes@naver.com>
2022-07-07 11:06:36 +0900

    lab1: implement nextNumber method for Collatz Sequence

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,17 +1,21 @@
-/** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+/**
+ * Class that prints the Collatz sequence starting from a given number.
+ *
+ * @author Seunghoon Oh
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /**
+     * takes the number n and return the next number according to Collatz Sequence
+     * if n is even, returns n/2
+     * if n is odd, returns 3*n + 1
+     */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
+        if (n % 2 == 0) {
+            return n / 2;
         }
+
+        return 3 * n + 1;
     }
 
     public static void main(String[] args) {
